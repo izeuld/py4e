28cfc7c8bccdd0e285e86078d63cb43664abadba
Karolina <izeuldwhitehands@gmail.com>
2018-12-27 18:41:00 +0100

    chapter 07 (iMac)

--- ex_07_00/ex_07_00.py
+++ ex_07_00/ex_07_00.py
@@ -8,3 +8,17 @@ print('Line Count:', count)
 # If you know the file is relatively small compared to the size of your main memory, you can read the whole file into one string using the read method on the file handle.
 fhand = open('mbox-short.txt')
 inp = fhand.read()
+
+# the newline character is a single character, separating the characters in the file into lines.
+stuff = 'A1\nA2'
+print(stuff)
+len(stuff)
+
+# To write a file, you have to open it with mode "w" as a second parameter. If the file already exists, opening it in write mode clears out the old data and starts fresh, so be careful! If the file doesn't exist, a new one is created.
+fout = open('mbox-short2.txt', 'w')
+print(fout)
+# The file object keeps track of where it is, so if you call write again, it adds the new data to the end. To manage the ends of lines as we write to the file, explicitly insert the newline character when ending a line.
+line2 = 'the emblem of our land.\n'
+fout.write(line2)
+# When you are done writing, close the file to make sure that the last bit of data is physically written to the disk.
+fout.close()
--- ex_07_01/ex_07_01.py
+++ ex_07_01/ex_07_01.py
@@ -1,1 +1,2 @@
 # Write a program to read through a file and print the contents of the file (line by line) all in upper case.
+fhand = open('../text/mbox-short.txt')
